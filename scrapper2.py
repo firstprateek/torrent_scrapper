790de78b1528242456c1fd123333233d47ed3940
Prateek Joshi <lastprateek@gmail.com>
2018-09-30 00:59:55 -0500

    escape unicode links

--- scrapper2.py
+++ scrapper2.py
@@ -11,7 +11,7 @@ import signal
 
 process_status={}
 CONFIG = {}
-
+processes_completed = 0
 #CONFIG SETTINGS
 with open("config.json") as jsonfile:
   CONFIG = json.load(jsonfile)
@@ -27,8 +27,8 @@ def kill_and_set_flag(proc, id):
 def convert_magnet_to_torrent(link_tag):
   torrent_file_name = "torrent%s" %(os.getpid())
   print("started subprocess: %s" %(torrent_file_name))
-
-  proc = Popen(['python', 'Magnet_To_Torrent2.py', '-m', "%s" %(link_tag['href']), '-o', torrent_file_name], stdout=PIPE, preexec_fn=os.setsid)
+  
+  proc = Popen(['python', 'Magnet_To_Torrent2.py', '-m', link_tag['href'].encode('ascii', 'ignore'), '-o', torrent_file_name], stdout=PIPE, preexec_fn=os.setsid)
   timer = Timer(CONFIG['timeout_sec'], kill_and_set_flag, [proc, torrent_file_name])
   result = []
   try:
@@ -47,6 +47,7 @@ def convert_magnet_to_torrent(link_tag):
       return result
 
 def scrape_page(url, csv_writer):
+  global processes_completed
   response = requests.get(url['link'], headers=CONFIG['headers'])
 
   if response.status_code != 200:
@@ -56,7 +57,6 @@ def scrape_page(url, csv_writer):
   soup = BeautifulSoup(response.text, 'html.parser')
   links = [link for link in soup.find_all('a') if link['href'].startswith('magnet')]
 
-  processes_completed = 0
   for link_index in range(0, len(links), CONFIG['process_count']):
     chunk = links[link_index:link_index+CONFIG['process_count']]
     pool = Pool(processes=CONFIG['process_count'])
@@ -74,7 +74,7 @@ def scrape_page(url, csv_writer):
           check_key(result['info'], 'name'),
           check_key(result, 'announce-list'),
           check_key(url, 'type'),
-          links[link_index+idx]['href'],
+          links[link_index+idx]['href'].encode('ascii', 'ignore'),
           check_key(url, 'source'),
         ]
       )
@@ -82,7 +82,7 @@ def scrape_page(url, csv_writer):
     processes_completed += CONFIG['process_count']
     print("processes completed: %s" %(processes_completed))
 
-def run_scrapper(url):
+if __name__ == '__main__':
   with open(CONFIG['csv_name'], 'wb') as myfile:
     csv_writer = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     csv_writer.writerow(
@@ -98,14 +98,11 @@ def run_scrapper(url):
         'source'
       ]
     )
-    
-    if url['multi_page']:
-      for i in range(1, url['pages']):
-        url['link'] = "%s%s" %(url['link'], i) if i > 1 else url['link']
-        scrape_page(url, csv_writer)
-    else:
-      scrape_page(url, csv_writer)
 
-if __name__ == '__main__':
-  for url in CONFIG['url_list']:
-    run_scrapper(url)
+    for url in CONFIG['url_list']:
+      if url['multi_page']:
+        for i in range(1, url['pages']):
+          url['link'] = "%s%s" %(url['link'], i) if i > 1 else url['link']
+          scrape_page(url, csv_writer)
+      else:
+        scrape_page(url, csv_writer)
